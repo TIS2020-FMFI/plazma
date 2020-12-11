98f8798229b15d452110b5d8f1c56cf61657f2a1
bforgacova <61239417+bforgacova@users.noreply.github.com>
2020-12-11 14:13:43 +0100

    Update project.py

--- src/project.py
+++ src/project.py
@@ -1,15 +1,27 @@
 class State:
-    def __init__(self):
-        self.stav = ""
+    def __init__(self, stav):
+        self.stav = stav
+
 
 
 class Calibration:
-    def __init__(self):
-        self.calibration = ""
-        self.typ = ""
+    def __init__(self, adapter):
+        self.calibration = None
+        self.typ = None
         self.port1 = 0
         self.port2 = 0
         self.velocity_factor = 0
+        self.adapter = adapter
+
+    def get_calibration(self):
+        self.adapter.get_calibration()
+
+    def set_calibration(self):
+       self.adapter.set_calibration(self)
+
+    def adjust_calibration(self, port1, port2, velocity):
+        ...
+
 
 
 class FileManager:
